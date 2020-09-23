5f4a1b9cae3a88ab2363687da1fd52089d4620b9
Eric Mariot <ericmariots@gmail.com>
2020-09-23 10:51:51 -0300

    Users modules injection and controllres

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,10 +1,7 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 // Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
 // Condições a mais do que a acima provavelmente será preciso abstrair em um service
@@ -12,28 +9,10 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 // Regra de negócio: Algo que está muito específico na aplicação
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
+const appointmentsController = new AppointmentsController();
 
 // aplicar este middleware em todas as rotas de agendamento
 appointmentsRouter.use(ensureAuthenticated);
-
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  // transformação de dado
-  const parsedDate = parseISO(date);
-
-  const createAppointment = new CreateAppointmentService(appointmentsRepository);
-
-  const appointment = await createAppointment.execute({ date: parsedDate, provider_id });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
