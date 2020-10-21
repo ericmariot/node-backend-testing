86074008fd34558dd176dd5cd8008468af7f0484
Eric Mariot <ericmariots@gmail.com>
2020-10-21 10:53:57 -0300

    mail ses working

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'mariot@ericarea.com',
+      name: 'Eric Area',
+    },
+  },
+} as IMailConfig;
